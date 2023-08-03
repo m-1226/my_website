f756198f60677e539ade2a7c3034a977a0f5268c
mahmoud <mahmoud55551226@yahoo.com>
2023-08-03 15:30:14 +0300

    organize projects through proejctsCard file

---
+++ src/pages/projectsCard.js
@@ -0,0 +1,24 @@
+export const projectsData = [
+    {
+        title: "ArabiaType",
+        img: "/post_img.webp",
+        desc: "The First minimalist, customizable Arabic Typing Test Website in the arabic world.",
+        url: "#",
+        badge: "Coming Soon 🔥",
+    },
+    {
+        title: "BIS Hub",
+        img: "/post_img.webp",
+        desc: "The first app that collects & orgainze the materials for BIS Studtens in one Hub.",
+        url: "#",
+        badge: "Coming Soon 🔥",
+    },
+    {
+        title: "Demo Project 3",
+        img: "/post_img.webp",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        url: "#",
+        badge: "test",
+    }
+
+];
